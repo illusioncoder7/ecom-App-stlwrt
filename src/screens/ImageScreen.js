322fbfa6d5f843588d3b5a03f807470b0941e2c7
sujan khadka <71214897+Blashes007@users.noreply.github.com>
2020-09-21 23:37:41 +0545

    Update ImageScreen.js
    name of image was wrong

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -5,7 +5,7 @@ const ImageScreen=()=>{
     return(
         <View>
             <Text style={{textAlign:'center', fontSize:30}}>This is image screen</Text>
-            <ImageDetail imgpoint={4} title='Forest' imgsrc={require('../../assests/Forest.jpg')}/>
+            <ImageDetail imgpoint={4} title='Forest' imgsrc={require('../../assests/forest.jpg')}/>
             {/* COMMENTING OUT SECTION BELOW CAUSE IT CAUSED ERROR THAT THE FILE ARE NOT FOUND*/}
             {/* <ImageDetail imgpoint={9} title='Mountain' imgsrc={require('../../assets/Mountain.jpg')}/>
             <ImageDetail imgpoint={7} title='Beach' imgsrc={require('../../assets/Beach.jpg')}/> */}
