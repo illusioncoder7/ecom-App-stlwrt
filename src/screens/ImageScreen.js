5b44691099eef1c1cde5d1131ffc0c03977f5ec0
subba <mandilsubba@gmail.com>
2020-09-24 12:05:31 +0545

    commented image source

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -5,11 +5,8 @@ const ImageScreen=()=>{
     return(
         <View>
             <Text style={{textAlign:'center', fontSize:30}}>This is image screen</Text>
-<<<<<<< HEAD
             {/* <ImageDetail imgpoint={4} title='Forest' imgsrc={require('../../assests/Forest.jpg')}/>
-=======
             <ImageDetail imgpoint={4} title='Forest' imgsrc={require('../../assests/forest.jpg')}/>
->>>>>>> 292be7f583f87eb8c4b1f4cd6860075e485bfae6
             {/* COMMENTING OUT SECTION BELOW CAUSE IT CAUSED ERROR THAT THE FILE ARE NOT FOUND*/}
             {/* <ImageDetail imgpoint={9} title='Mountain' imgsrc={require('../../assets/Mountain.jpg')}/>
             <ImageDetail imgpoint={7} title='Beach' imgsrc={require('../../assets/Beach.jpg')}/> */}
