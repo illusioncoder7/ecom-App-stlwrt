f014a06d08716ba5442416b25d0c0a5b9e351b3f
subba <mandilsubba@gmail.com>
2020-09-24 20:03:35 +0545

    added docker success

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import * as React from 'react';
 import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-// import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import PostScreen from './src/screens/PostScreen'; 
 import LoginScreen from './src/screens/LoginScreen';
@@ -14,32 +14,65 @@ import SignupScreen from './src/screens/SignupScreen';
 
  
  
-const Stack = createStackNavigator();
-// const Drawer = createDrawerNavigator();
+const HomeStack = createStackNavigator();
+const DetailStack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const HomeStackScreen =()=>(
+  <HomeStack.Navigator screenOptions={{
+    headerStyle:{
+      backgroundColor:'#10386e',
+    },
+    headerTintColor:'#fff',
+    headerTitleStyle:{
+      fontWeight:'bold'
+    }
+  }}>
+    <HomeStack.Screen 
+    name="Login" 
+    component={LoginScreen} 
+    options={{title:'Stalwart Engineering Solution'}}
+    />
+    
+    <HomeStack.Screen name="PostScreen" component={PostScreen} />
+     
+    <HomeStack.Screen name="Signup" component={SignupScreen} />
+
+  </HomeStack.Navigator >
+  
+)
+const DetailStackScreen =()=>(
+  <DetailStack.Navigator screenOptions={{
+    headerStyle:{
+      backgroundColor:'#10386e',
+    },
+    headerTintColor:'#fff',
+    headerTitleStyle:{
+      fontWeight:'bold'
+    }
+  }}>
+    <DetailStack.Screen 
+    name="Login" 
+    component={LoginScreen} 
+    options={{title:'Stalwart Engineering Solution'}}
+    />
+    
+    <DetailStack.Screen name="PostScreen" component={PostScreen} />
+     
+    <DetailStack.Screen name="Signup" component={SignupScreen} />
+
+  </DetailStack.Navigator >
+  
+)
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{
-        headerStyle:{
-          backgroundColor:'#10386e',
-        },
-        headerTintColor:'#fff',
-        headerTitleStyle:{
-          fontWeight:'bold'
-        }
-      }}>
-        <Stack.Screen 
-        name="Login" 
-        component={LoginScreen} 
-        options={{title:'Stalwart Engineering Solution'}}
-        />
-        
-        <Stack.Screen name="PostScreen" component={PostScreen} />
-         
-        <Stack.Screen name="Signup" component={SignupScreen} />
-
-      </Stack.Navigator>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeStackScreen} />
+        <Drawer.Screen name="Detail" component={DetailStackScreen} />
+      </Drawer.Navigator>
+     
     </NavigationContainer>
   );
 }
